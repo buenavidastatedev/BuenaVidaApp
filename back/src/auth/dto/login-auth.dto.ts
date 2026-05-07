2bfb3cde234fffaca6a829a61701cc7dfe1f5006
CarolinaAriza-mind <carolinaarizat93@gmail.com>
2026-05-07 18:47:21 -0300

    fix: configurando login con google

--- back/src/auth/dto/login-auth.dto.ts
+++ back/src/auth/dto/login-auth.dto.ts
@@ -2,18 +2,18 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginAuthDto {
-    @ApiProperty({
-        example: 'sasha@gmail.com',
-        description: 'Email del usuario.',
-    })
-    @IsEmail()
-    email!: string;
+  @ApiProperty({
+    example: 'sasha@gmail.com',
+    description: 'Email del usuario.',
+  })
+  @IsEmail()
+  email!: string;
 
-    @ApiProperty({
-        example: '12345678',
-        description: 'Contraseña del usuario.',
-    })
-    @IsString()
-    @MinLength(8)
-    password!: string;
+  @ApiProperty({
+    example: '12345678',
+    description: 'Contraseña del usuario.',
+  })
+  @IsString()
+  @MinLength(8)
+  password!: string;
 }
